9f3f840f800188e9adf138407c515ece0145417b
scattamooch <seanstevens729@gmail.com>
2023-07-08 18:17:47 -0400

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_comprehension = [n for n in num_list if n % 2 == 0]
+    if len(evens_comprehension) == 0:
+        return []
+    else:
+        return evens_comprehension
+    
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return []
+    else:
+        exclamations = [(string + "!") for string in sentence_list]
+        return exclamations
